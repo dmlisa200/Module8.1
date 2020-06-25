49936145d6f8a1d2f59663afea7b34637da59238
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-06-24 21:29:14 -0500

    Add files via upload

---
+++ datetime_assignment.py
@@ -0,0 +1,14 @@
+
+
+def half_birthday(yr, mo, d):
+    pass
+    """
+    birthday = date(yr, mo, d)
+    my_half_birthday = birthday + timedelta(days=183)
+    print("My birthday is: ", birthday)
+    print("My half birthday is: ", my_half_birthday)
+
+
+if __name__ == '__main__':
+    half_birthday(2020, 4, 20)
+    """
